200fd5d710880ec56cde45008778390f0b3d3cfa
Evan Sutherland <stackoverfloweth@gmail.com>
2023-03-23 17:55:54 -0500

    implemented lord level collections sdk

--- src/services/lordOfTheSDK.ts
+++ src/services/lordOfTheSDK.ts
@@ -1,9 +1,21 @@
-export type LordOfTheSDKOptions = {
-  apiKey?: string,
+import { BooksCollection } from '@/models/booksCollection'
+import { CharactersCollection } from '@/models/charactersCollection'
+import { MoviesCollection } from '@/models/moviesCollection'
+import { QuotesCollection } from '@/models/quotesCollection'
+import { ApiOptions } from '@/services'
+
+export type Store = {
+  books: BooksCollection,
+  characters: CharactersCollection,
+  movies: MoviesCollection,
+  quotes: QuotesCollection,
 }
 
-export class LordOfTheSDK {
-  public constructor(options: LordOfTheSDKOptions = {}) {
-    throw 'Not implemented'
+export function LordOfTheSdk(options: ApiOptions = {}): Store {
+  return {
+    books: new BooksCollection(options),
+    characters: new CharactersCollection(options),
+    movies: new MoviesCollection(options),
+    quotes: new QuotesCollection(options),
   }
 }
